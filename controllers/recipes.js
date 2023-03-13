e22baf602e98e538fae98c948a4365b3619be71b
ebmiller510 <llhappimess@gmail.com>
2023-03-13 17:43:54 -0500

    NEW ROUTE & CREATE ROUTE --GOOD

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -8,7 +8,7 @@ const router = express.Router()
 const db = require('../models')
 
 
-//index route 
+//index route----G2G
 router.get('/', function(req, res){
     db.Recipe.find({})
     .then(recipes => {
@@ -18,7 +18,49 @@ router.get('/', function(req, res){
     })
 })
 
-//get item by generated id 
+
+//to display all of the created recipes SHOW ROUTE-----G2G
+router.get('/', function(req, res){
+    db.Recipe.find({})
+        .then(recipes => {
+            res.render('recipe-index', {
+                Recipe: recipes
+            })
+        })
+})
+
+
+//NEW RECIPE ROUTE 
+router.get('/new', (req, res) => {
+    res.render('new-recipe')
+})
+
+//CREATE RECIPE ROUTE
+router.post('/', (req, res) => {
+    db.Recipe.create(req.body)
+    .then(recipe => {
+        res.redirect('/recipes/' + recipe._id)
+    })
+})
+
+
+
+
+//get item by generated id SHOW ROUTE---G2G
+router.get('/:id', function(req, res){
+
+    db.Recipe.findById(req.params.id)
+    .then(recipe => {
+        res.render('recipe-details', {
+            recipe: recipe
+        })
+    })        
+    .catch(() => res.send('404 Error: Page Not Found'))
+
+})
+
+
+//
 
 
 module.exports = router
